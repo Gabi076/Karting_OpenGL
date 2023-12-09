ca8a3ddd9007dc4a51157cc1fa5d86b84f18d833
DorinUscoiu1 <dorin.uscoiu@student.unitbv.ro>
2023-12-09 20:06:40 +0200

    Camera class

--- KartingSim/KartingSim.cpp
+++ KartingSim/KartingSim.cpp
@@ -1,7 +1,29 @@
-// KartingSim.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h> 
+
+#include <GL/glew.h>
+
+#define GLM_FORCE_CTOR_INIT 
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Camera.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+const unsigned int SCR_WIDTH = 1000;
+const unsigned int SCR_HEIGHT = 800;
+
 
 int main()
 {
