19da687ad2009ee0c4f65ac6c591bea9faf1d3c6
Secareanu David <david.secareanu@student.unitbv.ro>
2023-12-10 17:46:26 +0200

    render scene, render floor

--- KartingSimulator/test/KartingSim.cpp
+++ KartingSimulator/test/KartingSim.cpp
@@ -15,6 +15,7 @@
 #include <fstream>
 #include <sstream>
 #include "Camera.h"
+#include "Shader.h"
 #define STB_IMAGE_IMPLEMENTATION 
 #include"IncludeDir/stb-master/stb_image.h"
 
@@ -60,6 +61,51 @@ unsigned int CreateTexture(const std::string& strTexturePath)
 
     return textureId;
 }
+
+void renderScene(const Shader& shader)
+{
+    // floor
+    glm::mat4 model;
+    shader.SetMat4("model", model);
+    renderFloor();
+}
+
+unsigned int planeVAO = 0;
+void renderFloor()
+{
+    unsigned int planeVBO;
+
+    if (planeVAO == 0) {
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        float planeVertices[] = {
+            // positions            // normals         // texcoords
+            25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+            -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+            -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+
+            25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+            -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+            25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
+        };
+        // plane VAO
+        glGenVertexArrays(1, &planeVAO);
+        glGenBuffers(1, &planeVBO);
+        glBindVertexArray(planeVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        glBindVertexArray(0);
+    }
+
+    glBindVertexArray(planeVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
 
 int main(int argc, char** argv)
 {
