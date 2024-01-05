6d334b6932dc483e1d8124827857f99632888e65
Secareanu David <david.secareanu@student.unitbv.ro>
2024-01-05 18:43:05 +0200

    update

--- KartingSimulator/test/KartingSim.cpp
+++ KartingSimulator/test/KartingSim.cpp
@@ -124,6 +124,32 @@ unsigned int loadCubemap(vector<std::string> faces)
 	glGenTextures(1, &textureID);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
 
+	int width, height, nrChannels;
+	for (unsigned int i = 0; i < faces.size(); i++)
+	{
+		unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+			);
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+			stbi_image_free(data);
+		}
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	return textureID;
+}
+
 void renderScene(const Shader& shader)
 {
     // floor
@@ -455,6 +481,7 @@ int main(int argc, char** argv)
 
 		lightingShader.SetMat4("projection", pCamera->GetProjectionMatrix());
 		lightingShader.SetMat4("view", pCamera->GetViewMatrix());
+
 		kartModel = glm::scale(glm::mat4(1.0), glm::vec3(0.00046f));
 		kartModel = glm::translate(kartModel, kartPos) ;
 		kartModel = glm::rotate(kartModel, glm::radians(90.f) + glm::radians(-accumulatedRotation), glm::vec3(0.0f, 1.0f, 0.0f));
